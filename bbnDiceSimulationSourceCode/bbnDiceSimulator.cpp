3545b767e9670cc39216d6c935cbd9b86d592ac0
kraken05doo <kraken05doo@gmail.com>
2025-02-19 12:42:55 +0000

    updated source code, added multiple rolls with different pools

---
+++ bbnDiceSimulationSourceCode/bbnDiceSimulator.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d996b221777b2c03d9318fd70a98e1cd0d90cf20b2c4c8f7c363240c668c086d
+size 3643
---
+++ bbnDiceSimulationSourceCode/grapher.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3bbc3c6860a719ae8076d0d22356a4085cedc43ce35c09c891bf5edb4ac1b10
+size 3486
